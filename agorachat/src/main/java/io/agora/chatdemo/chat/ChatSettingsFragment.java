d7ca3c39d7faa134bf551848cd3e10ac96e9a102
neil <neil.liu@easemob.com>
2021-11-03 18:03:32 +0800

    Add ChatSettingsFragment.

---
+++ agorachat/src/main/java/io/agora/chatdemo/chat/ChatSettingsFragment.java
@@ -0,0 +1,29 @@
+package io.agora.chatdemo.chat;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import io.agora.chatdemo.base.BaseBottomSheetFragment;
+import io.agora.chatdemo.databinding.FragmentChatSettingsBinding;
+
+public class ChatSettingsFragment extends BaseBottomSheetFragment {
+    private FragmentChatSettingsBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentChatSettingsBinding.inflate(inflater);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}

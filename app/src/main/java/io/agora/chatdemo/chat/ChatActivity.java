2f2435358163526dfb9a44fd02bd9aa49ac444f8
neil <neil.liu@easemob.com>
2021-11-16 16:41:44 +0800

    Add refresh logic for ChatActivity.

--- app/src/main/java/io/agora/chatdemo/chat/ChatActivity.java
+++ app/src/main/java/io/agora/chatdemo/chat/ChatActivity.java
@@ -277,7 +277,18 @@ public class ChatActivity extends BaseInitActivity {
                 finish();
             }
         });
+        checkUnreadCount();
         setDefaultTitle();
+    }
+
+    /**
+     * If conversation's unread count is not 0, then should notify to refresh
+     */
+    private void checkUnreadCount() {
+        Conversation conversation = ChatClient.getInstance().chatManager().getConversation(conversationId);
+        if(conversation != null && conversation.getUnreadMsgCount() > 0) {
+            LiveDataBus.get().with(DemoConstant.MESSAGE_CHANGE_CHANGE).postValue(new EaseEvent(DemoConstant.MESSAGE_CHANGE_CHANGE, EaseEvent.TYPE.MESSAGE));
+        }
     }
 
     private void showSnackBar(String event) {

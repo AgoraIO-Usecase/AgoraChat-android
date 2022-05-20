c73db388e045a7ed76023bcab3c2498bf4c5967f
jinan <neil.liu@easemob.com>
2022-05-20 17:12:36 +0800

    Modify Application.mk to support ndk-19c.

--- app/jni/Application.mk
+++ app/jni/Application.mk
@@ -1,9 +1,7 @@
 #APP_OPTIM := debug
 #APP_ABI := arm64-v8a armeabi-v7a  x86 x86_64
 APP_ABI := arm64-v8a armeabi-v7a
-#APP_STL := stlport_static
-APP_STL := gnustl_static
 #高版本ndk
-#APP_STL := c++_static
+APP_STL := c++_static
 LOCAL_MULTILIB := 32
-APP_PLATFORM:=11
+APP_PLATFORM:=android-21
